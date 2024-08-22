5a2053e132db5db5796f2c797546636edf887102
ValentinLeroux8100 <74792769+ValentinLeroux8100@users.noreply.github.com>
2024-08-22 13:18:03 +0200

    ajout authentification

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,40 +2,71 @@
 
 namespace App\Controller;
 
-use App\Entity\Deck;
-use App\Form\DeckType;
 use App\Repository\DeckRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account', methods: ['GET', 'POST'])]
+    #[Route('/account', name: 'app_account', methods: ['GET'])]
     public function index(DeckRepository $repository): Response
     {
         $decks = $repository->findAll();
         return $this->render('account/index.html.twig', ["decks" => $decks]);
     }
 
-    #[Route('/account/new', name: 'app_account_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager): Response
+
+    #[Route(path: 'account/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $deck = new Deck;
-        $form = $this->createForm(DeckType::class, $deck);
+        $error = $authenticationUtils->getLastAuthenticationError();
 
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/account/register', name: 'app_account_register', methods: ['GET', 'POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        Security $security,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($deck);
-            $manager->flush();
 
-            return $this->redirectToRoute('app_account');
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $security->login($user, 'form_login', 'main');
         }
 
-        return $this->render('account/new.html.twig', [
-            'form' => $form,
+        return $this->render('account/register.html.twig', [
+            'registrationForm' => $form,
         ]);
     }
 }
--- src/Controller/DeckController.php
+++ src/Controller/DeckController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\DeckType;
+use App\Entity\Deck;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Deck;
 
 class DeckController extends AbstractController
 {
@@ -14,6 +17,27 @@ class DeckController extends AbstractController
     {
         return $this->render('deck/index.html.twig', [
             "deck" => $deck
+        ]);
+    }
+
+
+
+    #[Route('/deck/new', name: 'app_deck_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $deck = new Deck;
+        $form = $this->createForm(DeckType::class, $deck);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($deck);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('deck/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
